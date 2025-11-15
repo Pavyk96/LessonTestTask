4d9259398e62bb988d3c04efb2b799d704c9520f
Mezev Daniil <pepethefrog27@mail.com>
2025-11-15 22:47:08 +0500

    Исправил названия констант для вопросов

--- src/test/java/example/bot/TestCommandTest.java
+++ src/test/java/example/bot/TestCommandTest.java
@@ -12,10 +12,10 @@ import org.junit.jupiter.api.Assertions;
 class TestCommandTest {
 
     private static final String CMD_TEST = "/test";
-    private static final String Q1_TEXT = "Вычислите степень: 10^2";
-    private static final String Q1_ANS  = "100";
-    private static final String Q2_TEXT = "Сколько будет 2 + 2 * 2";
-    private static final String Q2_ANS = "6";
+    private static final String QUESTION_1_TEXT = "Вычислите степень: 10^2";
+    private static final String QUESTION_1_ANS  = "100";
+    private static final String QUESTION_2_TEXT = "Сколько будет 2 + 2 * 2";
+    private static final String QUESTION_2_ANS = "6";
 
     /**
      * Правильный ответ на вопрос + выводится следующий вопрос
@@ -30,20 +30,20 @@ class TestCommandTest {
         logic.processCommand(user, CMD_TEST);
 
         Assertions.assertEquals(
-                Q1_TEXT,
+                QUESTION_1_TEXT,
                 bot.lastMessage(chat),
                 "После команды /test ожидали первый вопрос"
         );
 
-        logic.processCommand(user, Q1_ANS);
+        logic.processCommand(user, QUESTION_1_ANS);
 
         Assertions.assertEquals(
-                Q2_TEXT,
+                QUESTION_2_TEXT,
                 bot.lastMessage(chat),
                 "После правильного ответа ожидали следующий вопрос"
         );
 
-        logic.processCommand(user, Q2_ANS);
+        logic.processCommand(user, QUESTION_2_ANS);
         Assertions.assertEquals(
                 "Тест завершен",
                 bot.lastMessage(chat),
@@ -64,7 +64,7 @@ class TestCommandTest {
         logic.processCommand(user, CMD_TEST);
         Assertions.assertEquals(State.TEST, user.getState());
         Assertions.assertEquals(
-                Q1_TEXT,
+                QUESTION_1_TEXT,
                 bot.lastMessage(chat),
                 "После команды /test ожидали первый вопрос"
         );
@@ -72,11 +72,11 @@ class TestCommandTest {
         logic.processCommand(user, "неверно");
 
         Assertions.assertTrue(
-                bot.hasMessage(chat, "Вы ошиблись, верный ответ: " + Q1_ANS),
+                bot.hasMessage(chat, "Вы ошиблись, верный ответ: " + QUESTION_1_ANS),
                 "Не верный вывод"
         );
         Assertions.assertEquals(
-                Q2_TEXT,
+                QUESTION_2_TEXT,
                 bot.lastMessage(chat),
                 "После неправильного ответа ожидали следующий вопрос"
         );
