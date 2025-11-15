206a3681fb55a290e822e72850b72eada7037b83
Mezev Daniil <pepethefrog27@mail.com>
2025-11-15 22:56:56 +0500

    Исправил названия констант для вопросов

--- src/test/java/example/bot/RepeatCommandTest.java
+++ src/test/java/example/bot/RepeatCommandTest.java
@@ -12,8 +12,8 @@ class RepeatCommandTest {
 
     private static final String CMD_TEST = "/test";
     private static final String CMD_REPEAT = "/repeat";
-    private static final String Q1_TEXT = "Вычислите степень: 10^2";
-    private static final String Q1_ANS  = "100";
+    private static final String QUESTION_1_TEXT = "Вычислите степень: 10^2";
+    private static final String QUESTION_1_ANS  = "100";
 
     /**
      * Нет вопросов для повторения
@@ -53,11 +53,11 @@ class RepeatCommandTest {
         logic.processCommand(user, CMD_REPEAT);
 
         Assertions.assertTrue(
-                bot.hasMessage(chat, Q1_TEXT),
+                bot.hasMessage(chat, QUESTION_1_TEXT),
                 "Нет вопроса для повторения"
         );
 
-        logic.processCommand(user, Q1_ANS);
+        logic.processCommand(user, QUESTION_1_ANS);
         logic.processCommand(user, CMD_REPEAT);
 
         Assertions.assertTrue(
@@ -78,7 +78,7 @@ class RepeatCommandTest {
         User user = new User(chat);
 
         logic.processCommand(user, CMD_TEST);
-        logic.processCommand(user, Q1_ANS);
+        logic.processCommand(user, QUESTION_1_ANS);
 
         logic.processCommand(user, CMD_REPEAT);
 
@@ -108,7 +108,7 @@ class RepeatCommandTest {
 
         logic.processCommand(user, CMD_REPEAT);
         Assertions.assertTrue(
-                bot.hasMessage(chat, Q1_TEXT),
+                bot.hasMessage(chat, QUESTION_1_TEXT),
                 "После неправильного ответа в режиме повторения вопрос должен остаться в очереди"
         );
     }
@@ -129,7 +129,7 @@ class RepeatCommandTest {
 
         logic.processCommand(user, CMD_REPEAT);
 
-        logic.processCommand(user, Q1_ANS);
+        logic.processCommand(user, QUESTION_1_ANS);
 
         logic.processCommand(user, CMD_REPEAT);
         Assertions.assertTrue(
